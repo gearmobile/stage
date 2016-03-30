6d718acf712d1f955e7a359a26b01b45689aac7d
Valery Semenencko <gearmobile@gmail.com>
2016-03-30 22:28:51 +0300

    fintess

--- fitness/markup/modules/header-fixed/header-fixed.js
+++ fitness/markup/modules/header-fixed/header-fixed.js
@@ -1,1 +1,11 @@
+const $topHeader = $('.header-fixed');
 
+$(window).on('scroll', () => {
+
+  if ( $(window).scrollTop() > 0 ) {
+    $topHeader.addClass('is-active');
+  } else {
+    $topHeader.removeClass('is-active');
+  }
+
+});
