986f7b6f748cce4487879db9c49caa6a1bea47ca
Valery Semenencko <gearmobile@gmail.com>
2016-04-27 23:34:54 +0300

    atlantida

--- atlantida/markup/modules/hero/hero.js
+++ atlantida/markup/modules/hero/hero.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+    const swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 30,
+        effect: 'fade'
+    });
+});
--- atlantida/markup/modules/top-header/top-header.js
+++ atlantida/markup/modules/top-header/top-header.js
@@ -1,9 +1,9 @@
-window.addEventListener('DOMContentLoaded', function () {
-    var html = document.documentElement;
-    var topHeader = document.querySelectorAll('.top-header');
-    if ( html.scrollTop > 0 ) {
-        topHeader.classList.add('is-fixed');
+const topHeader = $('.top-header');
+
+$(window).on('scroll', function () {
+    if ( $(window).scrollTop > 0 ) {
+        topHeader.addClass('is-fixed');
     } else {
-        topHeader.classList.remove('is-fixed');
+        topHeader.removeClass('is-fixed');
     }
 });
