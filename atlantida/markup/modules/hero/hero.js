e2e89cb210ac207ec9acbf84abfceef6ebf30caf
gearmobile <gearmobile@gmail.com>
2016-04-29 16:08:52 +0300

    atlantida

--- atlantida/markup/modules/hero/hero.js
+++ atlantida/markup/modules/hero/hero.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-    const swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 30,
-        effect: 'fade'
+    $('.hero__silder').slick({
+        dots: false
     });
 });
