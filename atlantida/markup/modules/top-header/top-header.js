2d07464c13a976ff08785435b0d133aa1e472581
Valery Semenencko <gearmobile@gmail.com>
2016-04-30 20:08:43 +0300

    atlantida

--- atlantida/markup/modules/top-header/top-header.js
+++ atlantida/markup/modules/top-header/top-header.js
@@ -1,9 +1,13 @@
-const topHeader = $('.top-header');
+$(function () {
 
-$(window).on('scroll', function () {
-    if ( $(window).scrollTop > 0 ) {
-        topHeader.addClass('is-fixed');
-    } else {
-        topHeader.removeClass('is-fixed');
-    }
+    const topHeader = $('.top-header');
+
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 0 ) {
+            topHeader.addClass('is-fixed');
+        } else {
+            topHeader.removeClass('is-fixed');
+        }
+    });
+
 });
