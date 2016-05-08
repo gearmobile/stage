8870ffd500bfdd83e9c4e0e4444b9e81e837ff2d
gearmobile <gearmobile@gmail.com>
2016-05-08 14:25:50 +0300

    atlantida

--- atlantida/markup/modules/testimonials/testimonials.js
+++ atlantida/markup/modules/testimonials/testimonials.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-    $('.testimonials__slider').slick({
+
+    const slickOptions = {
         dots: true,
         arrows: false,
         slidesToShow: 1,
-        infinite: true
-    });
+        slidesToScroll: 1,
+        autoplaySpeed: 1000,
+        // autoplay: true,
+        infinite: true,
+        customPaging: function () {
+            return '<a href="#"></a>';
+        }
+    };
+
+    $('.testimonials__slider').slick(slickOptions);
 });
