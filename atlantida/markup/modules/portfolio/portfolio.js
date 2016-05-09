0562c3eb1f1f01e6e13e0b55b69ccc4a50371618
Valery Semenencko <gearmobile@gmail.com>
2016-05-09 22:56:49 +0300

    atlantida

--- atlantida/markup/modules/portfolio/portfolio.js
+++ atlantida/markup/modules/portfolio/portfolio.js
@@ -1,1 +1,19 @@
 
+var $portfolioGrid = $('.portfolio-gallery');
+
+$portfolioGrid.masonry({
+    itemSelector: '.portfolio-gallery__item',
+    percentPosition: true,
+    columnWidth: '.portfolio-gallery__sizer',
+    layoutMode: 'fitRows'
+});
+
+$portfolioGrid.imagesLoaded().progress( function () {
+    $portfolioGrid.masonry();
+});
+
+$('.portfolio-filtering').on( 'click', 'a', function () {
+    var filterValue = $( this ).attr('data-filter');
+    // filterValue = filterFns[ filterValue ] || filterValue;
+    $portfolioGrid.isotope({ filter: filterValue });
+});
