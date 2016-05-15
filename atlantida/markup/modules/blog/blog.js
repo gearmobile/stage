baefabc89c34a1cfed5c9dc9fdeecfc5298e0b38
gearmobile <gearmobile@gmail.com>
2016-05-15 21:14:06 +0300

    atl

--- atlantida/markup/modules/blog/blog.js
+++
@@ -1,8 +0,0 @@
-$('.gallery').isotope({
-    itemSelector: '.gallery__item',
-    percentPosition: true,
-    masonry: {
-        columnWidth: '.gallery__item'
-    }
-});
-
--- atlantida/markup/modules/hero/hero.js
+++ atlantida/markup/modules/hero/hero.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-    //
+$('.slider').slick({
+    dots: false,
+    fade: true,
+    infinite: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<a href="#" class="slick-prev"></a>',
+    nextArrow: '<a href="#" class="slick-prev"></a>'
 });
