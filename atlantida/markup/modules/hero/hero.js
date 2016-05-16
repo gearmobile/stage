b3a8de4564f3f382c8b1f5c4ce14dc973490de68
gearmobile <gearmobile@gmail.com>
2016-05-16 11:06:42 +0300

    atlantida

--- atlantida/markup/modules/hero/hero.js
+++ atlantida/markup/modules/hero/hero.js
@@ -1,11 +1,30 @@
 $('.slider').slick({
     dots: false,
     fade: true,
-    infinite: true,
+    infinite: false,
     cssEase: 'linear',
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     prevArrow: '<a href="#" class="slick-prev"></a>',
-    nextArrow: '<a href="#" class="slick-next"></a>'
+    nextArrow: '<a href="#" class="slick-next"></a>',
+    responsive: [
+        {
+            breakpoint: 1110,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                fade: false
+            }
+        },
+        {
+            breakpoint: 400,
+            settings: {
+                fade: true,
+                autoplay: true,
+                autoplaySpeed: 2000
+            }
+        }
+    ]
 });
