44de0a6583778f7ee1bb58f39897f77a60e8fd83
gearmobile <gearmobile@gmail.com>
2016-05-17 10:17:31 +0300

    atlantida

--- atlantida/markup/modules/history/history.js
+++ atlantida/markup/modules/history/history.js
@@ -1,1 +1,8 @@
-
+$(document).ready( function () {
+    var showButton = $('.content__show');
+    var collapsedBlock = $('.is-collapsed');
+    showButton.on('click', function () {
+        collapsedBlock.removeClass('is-collapsed').addClass('is-opened');
+        showButton.parent().css('display', 'none');
+    });
+});
