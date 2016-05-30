0ea88a4e31fed025f43235b09605d775919f6e13
Valery Semenencko <gearmobile@gmail.com>
2016-05-30 22:06:28 +0300

    syntethica

--- synthetica/markup/modules/crew/crew.js
+++ synthetica/markup/modules/crew/crew.js
@@ -1,1 +1,21 @@
+$(document).ready(function () {
 
+    // BAR CHART ANIMATION
+    // -------------------------------------------
+
+    var blockSkills = $('.skills');
+    var bars = $('.skills__bar-value').width('0%');
+
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() < blockSkills.offset().top - $(window).height() * 0.75 ) {
+            bars.each( function () {
+                var value = $(this).data('value');
+                $(this).animate( { width: value + '%' }, { duration: 2000, easing: 'easeInCirc' } );
+            });
+        }
+    });
+
+
+
+
+});
--- synthetica/markup/modules/overview/overview.js
+++ synthetica/markup/modules/overview/overview.js
@@ -1,1 +1,22 @@
+$(document).ready( function () {
 
+    var counter = 0;
+    var blockOverview = $('.overview');
+    var overviewCounters = blockOverview.find('.overview__item-counter');
+    overviewCounters.text(counter);
+
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > blockOverview.offset().top - $(window).height() * 0.75 ) {
+            overviewCounters.each(function () {
+                var currentCounter = $(this);
+                $({ Counter: 0 }).animate( { Counter: currentCounter.attr('data-stop') }, {
+                    duration: 2000,
+                    easing: 'easeInSine',
+                    step: function (now) {
+                        currentCounter.text(Math.ceil(now));
+                    }} );
+            });
+            this.destroy();
+        }
+    });
+});
