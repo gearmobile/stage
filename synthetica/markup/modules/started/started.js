ef448ec3e0373d2a395ca9084e5350fba9730ea6
Valery Semenencko <gearmobile@gmail.com>
2016-05-31 23:03:07 +0300

    syntethica

--- synthetica/markup/modules/started/started.js
+++ synthetica/markup/modules/started/started.js
@@ -1,11 +1,1 @@
-$(document).ready( function () {
 
-    var blockStarted = $('.started');
-    var contentStarted = blockStarted.find('.started__center');
-
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > blockStarted.offset().top - $(window).height() * 0.5 ) {
-            contentStarted.addClass('animated fadeInUp');
-        }
-    });
-});
--- synthetica/markup/modules/topheader/topheader.js
+++ synthetica/markup/modules/topheader/topheader.js
@@ -1,23 +1,29 @@
 $(document).ready( function () {
 
+    // HIDE \ SHOW SECONDARY MENU TO RIGHT
+    // -----------------------------------------------------------------
     var topHeader = $('.topheader');
     var topHeaderNav = topHeader.find('.topheader__secondary-nav');
+    var topHeaderSecondarySubscribe = topHeader.find('.topheader__secondary-subscribe');
+    var topHeaderSecondarySearch = topHeader.find('.topheader__secondary-search');
+    var topHeaderSecondaryOpen = topHeader.find('.topheader__secondary-open');
+    var topHeaderSecondaryClose = topHeader.find('.topheader__secondary-close');
 
     if ( !topHeaderNav.hasClass('animated') ) {
         topHeaderNav.addClass('animated');
     }
 
     $(window).on('scroll', function () {
+        console.log($(window).scrollTop());
         if ( $(window).scrollTop() > 0 ) {
-            if ( topHeaderNav.hasClass('fadeInRight') ) {
-                topHeaderNav.removeClass('fadeInRight');
-            }
-            topHeaderNav.addClass('fadeOutRight');
-        } else {
+            topHeaderNav.removeClass('fadeInRight').addClass('fadeOutRight');
+        } else if ( $(window).scrollTop() === 0 ) {
             topHeaderNav.removeClass('fadeOutRight').addClass('fadeInRight');
         }
     });
 
+    // ANIMATE SCROLL
+    // -----------------------------------------------------------------
     $('a[href*="#"]:not([href="#"])').on('click', function () {
         if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
             var target = $(this.hash);
@@ -30,4 +36,43 @@ $(document).ready( function () {
             }
         }
     });
+
+    // SHOW \ HIDE SEARCH BLOCK
+    // -----------------------------------------------------------------
+    topHeaderSecondaryOpen.on('click', function () {
+        topHeaderSecondarySubscribe.removeClass('is-visible').addClass('is-hidden');
+        topHeaderSecondarySearch.removeClass('is-hidden').addClass('is-visible');
+        topHeaderNav.removeClass('fadeInRight').addClass('fadeOutRight');
+    });
+
+    topHeaderSecondaryClose.on('click', function () {
+        topHeaderSecondarySearch.removeClass('is-visible').addClass('is-hidden');
+        topHeaderSecondarySubscribe.removeClass('is-hidden').addClass('is-visible');
+        topHeaderNav.removeClass('fadeOutRight').addClass('fadeInRight');
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
