db6a88161b3263682b2967cf38e3075ee47c067d
gearmobile <gearmobile@gmail.com>
2016-06-03 11:08:21 +0300

    syntethica - like counter snippet

--- synthetica/markup/modules/freebies-overlay/freebies-overlay.js
+++ synthetica/markup/modules/freebies-overlay/freebies-overlay.js
@@ -1,1 +1,33 @@
+$(document).ready( function () {
 
+    // СЛУЧАЙНОЕ ЗНАЧЕНИЕ ДЛЯ СЧЕТЧИКА
+    // ---------------------------------------------
+
+    var likeCounters = $('.overlay__link-count');
+    likeCounters.each(function () {
+        var current = Math.floor( Math.random() * 100 + 1 );
+        $(this).text(current);
+    });
+
+    // СЧЕТЧИК И ИКОНКА
+    // ----------------------------------------------------
+
+    var likeLinks = $('.overlay__link-heart');
+    likeLinks.one('click', function () {
+
+        // увеличить значение счетчика на единицу при клике
+        // -----------------------------------------------------------
+        var currentCount = $(this).find('.overlay__link-count');
+        currentCount.html( currentCount.html() * 1 + 1 );
+
+        // изменить иконку при клике
+        // -----------------------------------------------------------
+        var currentIcon = $(this).find('.overlay-icon');
+        currentIcon.html('<use xlink:href="../svg-symbols.svg#heart-selected"></use>');
+    });
+
+    likeLinks.on('click', function (event) {
+        event.preventDefault();
+    });
+
+});
