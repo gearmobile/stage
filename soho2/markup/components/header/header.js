a6b354bc63ded3c68ff89646db63dac5d5a7053b
gearmobile <gearmobile@gmail.com>
2016-06-15 01:07:38 +0300

    soho2

---
+++ soho2/markup/components/header/header.js
@@ -0,0 +1,7 @@
+exports.slideout = ('./slideout');
+var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+});
---
+++ soho2/markup/static/js/main.js
@@ -0,0 +1,88 @@
+var Slideout = require('slideout');
+var Swiper = require('swiper');
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    var sidebar = document.querySelector('#sidebar');
+    var navbar = document.querySelector('#navbar');
+    var main = document.querySelector('#main');
+    var blockFixed = document.querySelector('.fixed');
+    var sidebarWidth = 256;
+    var navbarWidth = 256;
+    var btnSidebar = document.querySelector('.button-sidebar');
+    var btnNavbar = document.querySelector('.button-navbar');
+
+
+    var leftSlideout = new Slideout({
+        'panel': main,
+        'menu': sidebar,
+        'padding': sidebarWidth,
+        'tolerance': 70
+    });
+
+
+    var rightSlideout = new Slideout({
+        'panel': main,
+        'menu': navbar,
+        'padding': navbarWidth,
+        'tolerance': 70,
+        'side': 'right'
+    });
+
+
+    btnSidebar.addEventListener('click', function () {
+
+        leftSlideout.toggle();
+        sidebar.style.zIndex = '0';
+
+        leftSlideout.on('close', function () {
+            sidebar.style.zIndex = '-1';
+        });
+
+    });
+
+    leftSlideout.on('beforeopen', function () {
+        blockFixed.classList.add('fixed-open-left');
+    });
+
+    leftSlideout.on('beforeclose', function () {
+        blockFixed.classList.remove('fixed-open-left');
+    });
+
+
+    btnNavbar.addEventListener('click', function () {
+
+        rightSlideout.toggle();
+        navbar.style.zIndex = '0';
+
+        rightSlideout.on('close', function () {
+            navbar.style.zIndex = '-1';
+        });
+
+    });
+
+    rightSlideout.on('beforeopen', function () {
+        blockFixed.classList.add('fixed-open-right');
+    });
+
+    rightSlideout.on('beforeclose', function () {
+        blockFixed.classList.remove('fixed-open-right');
+    });
+
+
+    // PARALLAX WITH RELLAX.JS
+    // --------------------------------------------------
+    // var rellax = new Rellax('.main-article');
+
+
+    // SWIPER SLIDER
+    // --------------------------------------------------
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        autoplay: 3500
+    });
+
+
+}, false);
+
