14f99b433b1d8170d81f785f2bfa78c173b31f63
gearmobile <gearmobile@gmail.com>
2016-06-15 11:43:57 +0300

    soho

--- soho2/markup/components/header/header.js
+++
@@ -1,7 +0,0 @@
-exports.slideout = ('./slideout');
-var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70
-});
