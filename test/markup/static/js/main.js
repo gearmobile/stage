74b85d9b62539411e7003c81002bb84555683f03
gearmobile <gearmobile@gmail.com>
2016-06-27 16:33:48 +0300

    test

--- test/markup/static/js/main.js
+++ test/markup/static/js/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-/*
-    This file can be used as entry point for webpack!
- */
+var menuCurrency = document.querySelector( '#header-currency' );
+var menuCurrencyList = document.querySelector( '#header-currency-list' );
+var menuLanguage = document.querySelector( '#header-language' );
+var menuLanguageList = document.querySelector( '#header-language-list' );
+
+
+// OPEN LANGUAGE MENU
+// ----------------------------------------------------------
+menuLanguage.addEventListener( 'click', function ( event ) {
+    event.preventDefault();
+    menuLanguageList.classList.add('is-visible');
+}, false );
+
+// OPEN CURRENCY MENU
+// ----------------------------------------------------------
+menuCurrency.addEventListener( 'click', function ( event ) {
+    event.preventDefault();
+    menuCurrencyList.classList.add('is-visible');
+    //var className = menuCurrencyList.className;
+    //console.log(className);
+}, false );
+
+document.addEventListener( 'click', function ( event ) {
+    if ( event.target.id !== menuCurrencyList.id ) {
+        //console.log(menuCurrencyList.id);
+        menuCurrencyList.classList.remove('is-visible');
+    }
+});
+
+//console.log(menuCurrencyList.className);
