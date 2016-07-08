750b7d64fa460164a0216f2b36e67ab87f5280fa
gearmobile <gearmobile@gmail.com>
2016-07-08 11:10:28 +0300

    ef

--- ef/markup/static/js/main.js
+++ ef/markup/static/js/main.js
@@ -375,7 +375,38 @@ window.addEventListener( 'DOMContentLoaded', function () {
 
     // GALLERY SHOW MORE BUTTON
     // ------------------------------------------------------
+    var gridBtn = document.querySelector( '.recent-grid__button' );
+    var numberRows = 0;
+    var item;
+    var numberRowsCounter = 4;
+    var gridItems = document.querySelectorAll( '.recent-grid__item' );
 
+    gridBtn.addEventListener( 'click', function () {
+
+        var elems = [];
+
+        if ( window.innerWidth >= 980 ) {
+            for ( var i = 0; i < 3; i++ ) {
+                item = gridItems[ Math.floor( Math.random() * gridItems.length ) ].cloneNode( true );
+                elems.push( item );
+            }
+        } else if ( window.innerWidth < 980 ) {
+            for ( var j = 0; j < 2; j++ ) {
+                item = gridItems[ Math.floor( Math.random() * gridItems.length ) ].cloneNode( true );
+                elems.push( item );
+            }
+        }
+
+        iso.insert( elems );
+        ++numberRows;
+
+        if ( numberRows >= numberRowsCounter ) {
+            document.querySelector( '.recent-grid__main' ).style.marginBottom = 0;
+            gridBtn.onclick = null;
+            gridBtn.style.display = 'none';
+        }
+
+    }, false );
 
 
 
