0e1cad8806adb3c391a7b0ebb95a7828e9d9d86f
Valery Semenencko <gearmobile@gmail.com>
2016-07-09 20:52:21 +0300

    ef

--- ef/markup/static/js/main.js
+++ ef/markup/static/js/main.js
@@ -410,6 +410,28 @@ window.addEventListener( 'DOMContentLoaded', function () {
     }, false );
 
 
+
+    // SWIPER SLIDER TESTIMONIALS
+    // ------------------------------------
+    swiper( '.testimonials .swiper-container', {
+        pagination: '.testimonials .swiper-pagination',
+        paginationClickable: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        loop: true,
+        breakpoints: {
+            980: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 1
+            }
+        }
+    });
+
+
+
     // SWIPER SLIDER CITATION
     // ------------------------------------
     swiper( '.citation .swiper-container', {
