71347d7bf03a5905b87a39be0b4b04037a16c2b5
gearmobile <gearmobile@gmail.com>
2016-07-22 14:23:44 +0300

    talant - topheader section fix

--- talant/markup/static/js/main.js
+++ talant/markup/static/js/main.js
@@ -6,6 +6,24 @@ var imagesload = require('imagesloaded');
 var Isotope = require('isotope-layout');
 
 
+// TOPHEADER HIDE AND SHOW
+// ------------------------------------
+
+var topHeader = document.querySelector( '.topheader' );
+//var topHeaderMenuPrimary = document.querySelector( '.topheader__menu--primary' );
+var topOffset = 110;
+
+window.addEventListener( 'scroll', function () {
+    var scrollOffset = window.pageYOffset || document.documentElement.scrollTop;
+    if ( scrollOffset > topOffset ) {
+        topHeader.classList.add('is-hidden');
+    } else {
+        topHeader.classList.remove('is-hidden');
+    }
+}, false );
+
+
+
 
 
 
