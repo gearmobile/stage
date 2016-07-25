8dae20356601716252771c54cf68c9fcb80426f5
gearmobile <gearmobile@gmail.com>
2016-07-25 11:19:49 +0300

    talant - start block clients

--- talant/markup/static/js/main.js
+++ talant/markup/static/js/main.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var swiper = require('swiper');
-var GoogleMapsLoader = require('google-maps');
-var imagesload = require('imagesloaded');
-var Isotope = require('isotope-layout');
+var swiper = require( 'swiper' );
+var GoogleMapsLoader = require( 'google-maps' );
+var imagesload = require( 'imagesloaded' );
+var Isotope = require( 'isotope-layout' );
 
 
 // TOPHEADER HIDE AND SHOW
 // ------------------------------------
 
-var topHeader = document.querySelector( '.topheader' );
-var topOffset = 110;
-
-window.addEventListener( 'scroll', function () {
-    var scrollOffset = window.pageYOffset || document.documentElement.scrollTop;
-    if ( scrollOffset > topOffset ) {
-        topHeader.classList.add( 'is-hidden' );
-    } else {
-        topHeader.classList.remove( 'is-hidden' );
-    }
-}, false );
+//var topHeader = document.querySelector( '.topheader' );
+//var topOffset = 110;
+//
+//window.addEventListener( 'scroll', function () {
+//    var scrollOffset = window.pageYOffset || document.documentElement.scrollTop;
+//    if ( scrollOffset > topOffset ) {
+//        topHeader.classList.add( 'is-hidden' );
+//    } else {
+//        topHeader.classList.remove( 'is-hidden' );
+//    }
+//}, false );
 
 
 // PORTFOLIO
@@ -55,7 +55,38 @@ portfolioControl.addEventListener( 'click', function ( event ) {
 });
 
 
+// BLOG GRID
+// ------------------------------------
 
+function equalHeight( items ) {
+
+    // MAX HEIGHT VALUE
+    var maxHeight = 0;
+
+    // GET ALL LIST ITEMS
+    var listItems = document.querySelectorAll( items );
+
+    // GET TALLEST LIST ITEM VALUE
+    for ( var i = 0; i < listItems.length; i++ ) {
+        listItems[i].style.height = 'auto';
+        var listItemHeight = listItems[i].offsetHeight;
+        maxHeight = maxHeight < listItemHeight ? listItemHeight : maxHeight;
+    }
+
+    // SET HEIGHT FOR ALL LIST ITEMS
+    for ( var j = 0; j < listItems.length; j++ ) {
+        listItems[j].style.height = maxHeight + 'px';
+    }
+
+}
+
+window.addEventListener( 'load', function () {
+    equalHeight( '.blog__item' );
+}, false );
+
+window.addEventListener( 'resize', function () {
+    equalHeight( '.blog__item' );
+}, false );
 
 
 
