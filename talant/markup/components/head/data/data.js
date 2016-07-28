d605a16dc318c9d730dbf818d278f1f2abf9d943
gearmobile <gearmobile@gmail.com>
2016-07-28 13:39:10 +0300

    talant - start page about

--- talant/markup/components/head/data/data.js
+++ talant/markup/components/head/data/data.js
@@ -2,5 +2,9 @@ head: {
     defaults: {
         title: 'Atlantida',
         useSocialMetaTags: true
+    },
+    about: {
+        title: 'Atlantida - About',
+        useSocialMetaTags: true
     }
 }
