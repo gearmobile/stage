c1d32d7d6815ea451cba6c992fe7b0ac96c3339d
gearmobile <gearmobile@gmail.com>
2016-08-17 20:24:24 +0300

    pink

--- pink/markup/components/block-header/block-header.js
+++ pink/markup/components/block-header/block-header.js
@@ -1,1 +1,12 @@
-
+window.addEventListener( 'load', function () {
+    //---------------------------
+    // variables
+    // --------------------------
+    const headerImage = document.querySelector( '.header .header__image' );
+    const headerCenter = document.querySelector( '.header .center' );
+    const headerContent = document.querySelector( '.header .header__content' );
+    const mobileWidth = 500;
+    // --------------------------
+    //
+    // --------------------------
+}, false );
