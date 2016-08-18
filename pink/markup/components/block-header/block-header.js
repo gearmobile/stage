7ed626b4f0a11347e056903b1f9c4e8ad392b2f4
gearmobile <gearmobile@gmail.com>
2016-08-18 13:56:04 +0300

    pink - block reviews

--- pink/markup/components/block-header/block-header.js
+++ pink/markup/components/block-header/block-header.js
@@ -1,17 +1,14 @@
+//---------------------------
+// variables
+// --------------------------
+let center = document.querySelector( '.header__primary > .center' );
+let headerSecondary = document.querySelector( '.header__secondary' );
+const mobileWidth = 500;
+let flag = true;
+// ----------------------------
 window.addEventListener( 'load', function () {
-    //---------------------------
-    // variables
-    // --------------------------
-    let center = document.querySelector( '.header__primary > .center' );
-    let headerSecondary = document.querySelector( '.header__secondary' );
-    const mobileWidth = 500;
-    let flag = true;
-    // --------------------------
-    // check and replace
-    // --------------------------
-    if ( window.innerWidth > mobileWidth && flag ) {
+    if ( window.innerWidth > mobileWidth ) {
         center.insertBefore( headerSecondary.cloneNode( true ), center.childNodes[0] );
         headerSecondary.parentNode.removeChild( headerSecondary );
-        flag = false;
     }
 }, false );
--- pink/markup/components/block-reviews/block-reviews.js
+++ pink/markup/components/block-reviews/block-reviews.js
@@ -2,6 +2,14 @@
 const swiper = require('swiper');
 
 swiper( '.reviews .swiper-container', {
+    pagination: '.swiper-pagination',
     nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev'
+    prevButton: '.swiper-button-prev',
+    speed: 800,
+    breakpoints: {
+        768: {
+            autoplay: 4000,
+            grabCursor: true
+        }
+    }
 });
