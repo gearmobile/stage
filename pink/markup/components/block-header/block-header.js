6409a81b31208fe04a32a26cec0d083460558bae
gearmobile <gearmobile@gmail.com>
2016-08-18 11:09:22 +0300

    pink

--- pink/markup/components/block-header/block-header.js
+++ pink/markup/components/block-header/block-header.js
@@ -2,11 +2,16 @@ window.addEventListener( 'load', function () {
     //---------------------------
     // variables
     // --------------------------
-    const headerImage = document.querySelector( '.header .header__image' );
-    const headerCenter = document.querySelector( '.header .center' );
-    const headerContent = document.querySelector( '.header .header__content' );
+    let center = document.querySelector( '.header__primary > .center' );
+    let headerSecondary = document.querySelector( '.header__secondary' );
     const mobileWidth = 500;
+    let flag = true;
     // --------------------------
-    //
+    // check and replace
     // --------------------------
+    if ( window.innerWidth > mobileWidth && flag ) {
+        center.insertBefore( headerSecondary.cloneNode( true ), center.childNodes[0] );
+        headerSecondary.parentNode.removeChild( headerSecondary );
+        flag = false;
+    }
 }, false );
