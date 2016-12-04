c5f8af8de2981bc34c49f5bfa31e5857624252c6
gearmobile <gearmobile@gmail.com>
2016-12-04 20:43:33 +0300

    ef - change chart.js by chartist

--- ef/markup/components/block-skills/block-skills.js
+++ ef/markup/components/block-skills/block-skills.js
@@ -1,177 +1,80 @@
-const Chart = require('chart.js');
-// -------------------------------------------------
+const Chartist = require( 'chartist' );
+
 // VARIABLES
 // -------------------------------------------------
-const color1 = '#0cf';
-const color2 = '#eaeaea';
+
+// DONUT SKILLS VALUES
+// -----------------------------------------------------
+const donutUnoSkill = 58;
+const donutDuoSkill = 75;
+const donutTreSkill = 80;
+const donutQuattroSkill = 90;
+
+// DONUT ELEMENTS
+// -----------------------------------------------------
+const donutUno = document.querySelector( '#donutUno' );
+const donutDuo = document.querySelector( '#donutDuo' );
+const donutTre = document.querySelector( '#donutTre' );
+const donutQuattro = document.querySelector( '#donutQuattro' );
+
 const colorStroke = '#f00';
-const wpValue = 58;
-const htmlValue = 75;
-const cssValue = 80;
-const ptValue = 90;
-const cutoutValue = 82;
-// -------------------------------------------------
-window.addEventListener( 'DOMContentLoaded', function () {
-    // -------------------------------------------------
-    const wpElement = document.querySelector( '#wordpress' );
-    const htmlElement = document.querySelector( '#html' );
-    const cssElement = document.querySelector( '#css' );
-    const ptElement = document.querySelector( '#photoshop' );
-    // -------------------------------------------------
-    const wpContext = wpElement.getContext( '2d' );
-    const htmlContext = htmlElement.getContext( '2d' );
-    const cssContext = cssElement.getContext( '2d' );
-    const ptContext = ptElement.getContext( '2d' );
 
-    // -------------------------------------------------
-    // CHARTS GLOBAL CONFIG  ---------------------------
-    // -------------------------------------------------
-    Chart.defaults.global.showTooltips = false;
-    Chart.defaults.global.legend.display = false;
-    Chart.defaults.global.title.display = false;
-    Chart.defaults.global.animation.easing = 'easeOutQuart';
-    // -------------------------------------------------
-    // CHARTS DATA  ------------------------------------
-    // -------------------------------------------------
-    const wpData = {
-        labels: [ 'Skill', 'No Skill' ],
-        datasets: [
-            {
-                data: [ wpValue, 100 - wpValue ],
-                backgroundColor: [ color1, color2 ]
-            }
-        ]
-    };
-    const htmlData = {
-        labels: [ 'Skill', 'No Skill' ],
-        datasets: [
-            {
-                data: [ htmlValue, 100 - htmlValue ],
-                backgroundColor: [ color1, color2 ]
-            }
-        ]
-    };
-    const cssData = {
-        labels: [ 'Skill', 'No Skill' ],
-        datasets: [
-            {
-                data: [ cssValue, 100 - cssValue ],
-                backgroundColor: [ color1, color2 ]
-            }
-        ]
-    };
-    const ptData = {
-        labels: [ 'Skill', 'No Skill' ],
-        datasets: [
-            {
-                data: [ ptValue, 100 - ptValue ],
-                backgroundColor: [ color1, color2 ]
-            }
-        ]
-    };
-    // -------------------------------------------------
-    // CHARTS OPTIONS
-    // -------------------------------------------------
-    const wpOptions = {
-        cutoutPercentage: cutoutValue,
-        borderWidth: 3,
-        borderColor: colorStroke,
-        responsive: true,
-        animation: {
-            onComplete: function () {
-                wpContext.fillStyle = '#333';
-                wpContext.font = '3rem Lato, sans-serif';
-                wpContext.textBaseline = 'middle';
-                wpContext.textAlign = 'center';
-                wpContext.fillText( wpData.datasets[0].data[0] + '%', wpElement.width / 2, wpElement.height / 2 );
-            }
-        }
-    };
-    const htmlOptions = {
-        cutoutPercentage: cutoutValue,
-        borderWidth: 3,
-        borderColor: colorStroke,
-        responsive: true,
-        animation: {
-            onComplete: function () {
-                htmlContext.fillStyle = '#333';
-                htmlContext.font = '3rem Lato, sans-serif';
-                htmlContext.textBaseline = 'middle';
-                htmlContext.textAlign = 'center';
-                htmlContext.fillText( htmlData.datasets[0].data[0] + '%', htmlElement.width / 2, htmlElement.height / 2 );
-            }
-        }
-    };
-    const cssOptions = {
-        cutoutPercentage: cutoutValue,
-        borderWidth: 3,
-        borderColor: colorStroke,
-        responsive: true,
-        animation: {
-            onComplete: function () {
-                cssContext.fillStyle = '#333';
-                cssContext.font = '3rem Lato, sans-serif';
-                cssContext.textAlign = 'center';
-                cssContext.textBaseline = 'middle';
-                cssContext.fillText( cssData.datasets[0].data[0] + '%', cssElement.width / 2, cssElement.height / 2 );
-            }
-        }
-    };
-    const ptOptions = {
-        cutoutPercentage: cutoutValue,
-        borderWidth: 3,
-        borderColor: colorStroke,
-        responsive: true,
-        animation: {
-            onComplete: function () {
-                ptContext.fillStyle = '#333';
-                ptContext.font = '3rem Lato, sans-serif';
-                ptContext.textAlign = 'center';
-                ptContext.textBaseline = 'middle';
-                ptContext.fillText( ptData.datasets[0].data[0] + '%', ptElement.width / 2, ptElement.height / 2 );
-            }
-        }
-    };
-    // -------------------------------------------------
-    // CHARTS  -----------------------------------------
-    // -------------------------------------------------
-    var wpChart = new Chart( wpContext, {
-        type: 'doughnut',
-        data: wpData,
-        options: wpOptions
-    });
+// DONUT DATAS
+// -----------------------------------------------------
+const donutUnoData = {
+    labels: [ 'Skill', 'No skill' ],
+    series: [ donutUnoSkill, 100 - donutUnoSkill ]
+};
+const donutDuoData = {
+    labels: [ 'Skill', 'No skill' ],
+    series: [ donutDuoSkill, 100 - donutDuoSkill ]
+};
+const donutTreData = {
+    labels: [ 'Skill', 'No skill' ],
+    series: [ donutTreSkill, 100 - donutTreSkill ]
+};
+const donutQuattroData = {
+    labels: [ 'Skill', 'No skill' ],
+    series: [ donutQuattroSkill, 100 - donutQuattroSkill ]
+};
 
-    var htmlChart = new Chart( htmlContext, {
-        type: 'doughnut',
-        data: htmlData,
-        options: htmlOptions
-    });
+// DONUT OPTIONS
+// -----------------------------------------------------
+const donutOptions = {
+    donut: true,
+    showLabel: false,
+    donutWidth: 20
+};
 
-    var cssChart = new Chart( cssContext, {
-        type: 'doughnut',
-        data: cssData,
-        options: cssOptions
-    });
+window.addEventListener( 'load', function () {
 
-    var ptChart = new Chart( ptContext, {
-        type: 'doughnut',
-        data: ptData,
-        options: ptOptions
-    });
-    // -------------------------------------------------
+    // DRAW DONUT CHARTS
+    // -----------------------------------------------------
+    const chartUno = new Chartist.Pie( donutUno, donutUnoData, donutOptions );
+    const chartDuo = new Chartist.Pie( donutDuo, donutDuoData, donutOptions );
+    const chartTre = new Chartist.Pie( donutTre, donutTreData, donutOptions );
+    const chartQuattro = new Chartist.Pie( donutQuattro, donutQuattroData, donutOptions );
+
+    // ADD DONUT VALUES
+    // -----------------------------------------------------
+    const chartUnoValue = document.createElement( 'div' );
+    chartUnoValue.classList.add( 'holder-value' );
+    chartUnoValue.innerHTML = donutUnoData.series[0] + '%';
+    donutUno.appendChild( chartUnoValue );
+    // -----------------------------------------------------
+    const chartDuoValue = document.createElement( 'div' );
+    chartDuoValue.classList.add( 'holder-value' );
+    chartDuoValue.innerHTML = donutDuoData.series[0] + '%';
+    donutDuo.appendChild( chartDuoValue );
+    // -----------------------------------------------------
+    const chartTreValue = document.createElement( 'div' );
+    chartTreValue.classList.add( 'holder-value' );
+    chartTreValue.innerHTML = donutTreData.series[0] + '%';
+    donutTre.appendChild( chartTreValue );
+    // -----------------------------------------------------
+    const chartQuattroValue = document.createElement( 'div' );
+    chartQuattroValue.classList.add( 'holder-value' );
+    chartQuattroValue.innerHTML = donutQuattroData.series[0] + '%';
+    donutQuattro.appendChild( chartQuattroValue );
+
 }, false );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
