540708ac2a1db3bd065930aaec4b64a0ec06e2f0
gearmobile <gearmobile@gmail.com>
2017-01-05 22:43:28 +0300

    eve - add icons collapse change of accordion

--- eventide-bootstrap/js/app.js
+++ eventide-bootstrap/js/app.js
@@ -3,14 +3,18 @@ $( document ).ready( function() {
     // BLOCK EQUAL HEIGHT
     $( '.block-equal' ).matchHeight();
 
-    //
-    $('.panel-title__link').on('shown.bs.collapse', function () {
-        $(".fa").removeClass("fa-minus-circle").addClass("fa-plus-circle");
+    // accordion icons change
+    var blocks = $( '.tab-content .collapse' );
+    blocks.on( 'shown.bs.collapse', function () {
+        $( this ).prev().find( '.fa' )
+            .removeClass( 'fa-plus-circle' )
+            .addClass( 'fa-minus-circle' );
     });
 
-    $('.panel-title__link').on('hidden.bs.collapse', function () {
-        $(".fa").removeClass("fa-plus-circle").addClass("fa-minus-circle");
+    blocks.on( 'hidden.bs.collapse', function () {
+        $( this ).prev().find( '.fa' )
+            .removeClass( 'fa-minus-circle' )
+            .addClass( 'fa-plus-circle' );
     });
-
 
 });
