abcba1621b1989c16e0db3a7622fd07abf0dfaac
gearmobile <gearmobile@gmail.com>
2017-01-05 21:09:01 +0300

    eve

--- eventide-bootstrap/js/app.js
+++ eventide-bootstrap/js/app.js
@@ -3,4 +3,14 @@ $( document ).ready( function() {
     // BLOCK EQUAL HEIGHT
     $( '.block-equal' ).matchHeight();
 
+    //
+    $('.panel-title__link').on('shown.bs.collapse', function () {
+        $(".fa").removeClass("fa-minus-circle").addClass("fa-plus-circle");
+    });
+
+    $('.panel-title__link').on('hidden.bs.collapse', function () {
+        $(".fa").removeClass("fa-plus-circle").addClass("fa-minus-circle");
+    });
+
+
 });
