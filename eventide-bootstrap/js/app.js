8975c7fd1ff68606bad4a078fa8fc1da02fda7d6
gearmobile <gearmobile@gmail.com>
2017-01-06 13:28:24 +0300

    eve  -add jquery support highlight captions

--- eventide-bootstrap/js/app.js
+++ eventide-bootstrap/js/app.js
@@ -3,7 +3,7 @@ $( document ).ready( function() {
     // BLOCK EQUAL HEIGHT
     $( '.block-equal' ).matchHeight();
 
-    // accordion icons change
+    // ACCORDION ICONS CHANGE --------------------------------
     var blocks = $( '.tab-content .collapse' );
     blocks.on( 'shown.bs.collapse', function () {
         $( this ).prev().find( '.fa' )
@@ -16,5 +16,18 @@ $( document ).ready( function() {
             .removeClass( 'fa-minus-circle' )
             .addClass( 'fa-plus-circle' );
     });
+    // --------------------------------------------------------
+
+    // TITLE HIGHLIGHT
+    var captions = $( '.feature-two .flipper' );
+    captions.hover(
+        function () {
+            $( this ).next( '.thumbnail__title' ).addClass( 'thumbnail__title--highlight' );
+        },
+        function () {
+            $( this ).next( '.thumbnail__title' ).removeClass( 'thumbnail__title--highlight' );
+        }
+    );
+    // ---------------------------------------------------------
 
 });
