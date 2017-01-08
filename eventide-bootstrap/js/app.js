82466bb4c7409c07e01baec86dda893e7fe00db4
gearmobile <gearmobile@gmail.com>
2017-01-08 16:17:06 +0300

    eve - google map fix

--- eventide-bootstrap/js/app.js
+++ eventide-bootstrap/js/app.js
@@ -63,77 +63,8 @@ $( document ).ready( function() {
     // GOOGLE MAP ----------------------------------------------
 
     var brampton = document.querySelector( '#map' );
-    var bramptonCoords = new google.maps.LatLng( 51.5220535, 0.0419289 );
-    var zoomValue = 15;
-    var bramptonMapStyles = [
-        {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{color: '#e9e9e9'}, {lightness: 17}]
-        },
-        {
-            featureType: 'landscape',
-            elementType: 'geometry',
-            stylers: [{color: '#f5f5f5'}, {lightness: 20}]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry.fill',
-            stylers: [{color: '#ffffff'}, {lightness: 17}]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{color: '#ffffff'}, {lightness: 29}, {weight: 0.2}]
-        },
-        {
-            featureType: 'road.arterial',
-            elementType: 'geometry',
-            stylers: [{color: '#ffffff'}, {lightness: 18}]
-        },
-        {
-            featureType: 'road.local',
-            elementType: 'geometry',
-            stylers: [{color: '#ffffff'}, {lightness: 16}]
-        },
-        {
-            featureType: 'poi',
-            elementType: 'geometry',
-            stylers: [{color: '#f5f5f5'}, {lightness: 21}]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{color: '#dedede'}, {lightness: 21}]
-        },
-        {
-            elementType: 'labels.text.stroke',
-            stylers: [{visibility: 'on'}, {color: '#ffffff'}, {lightness: 16}]
-        },
-        {
-            elementType: 'labels.text.fill',
-            stylers: [{saturation: 36}, {color: '#333333'}, {lightness: 40}]
-        },
-        {
-            elementType: 'labels.icon',
-            stylers: [{visibility: 'off'}]
-        },
-        {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{ color: '#f2f2f2' }, { lightness: 19 }]
-        },
-        {
-            featureType: 'administrative',
-            elementType: 'geometry.fill',
-            stylers: [{ color: '#fefefe' }, { lightness: 20 }]
-        },
-        {
-            featureType: 'administrative',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#fefefe' }, { lightness: 17 }, { weight: 1.2 }]
-        }
-    ];
+    var bramptonCoords = new google.maps.LatLng( -33.86455, 151.209 );
+    var zoomValue = 12;
     var bramptonProperties = {
         center: bramptonCoords,
         zoom: zoomValue,
@@ -148,8 +79,7 @@ $( document ).ready( function() {
         verviewMapControl: true,
         overviewMapControlOptions: {
             opened: false
-        },
-        styles: bramptonMapStyles
+        }
     };
     var bramptonMap = new google.maps.Map( brampton, bramptonProperties );
     var bramptonMarker = new google.maps.Marker({
